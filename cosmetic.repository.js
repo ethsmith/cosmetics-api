3df819c1fcfb47dd5bc8f86674f4df17f88423c6
ethsmith <ethan.smith1845@gmail.com>
2023-06-24 12:26:45 -0500

    Add current cosmetic repository changes for now.

--- cosmetic.repository.js
+++ cosmetic.repository.js
@@ -2,10 +2,20 @@ const db = require('./db')
 
 async function getAllCosmetics() {
     const query = 'SELECT * FROM cosmetics';
-    await db.getConnection.query(query, (err, results) => {
-        if (err) {
-            throw err;
-        }
-        return results.json;
-    });
+    const result = await db.getConnection.query(query);
+    return result.rows;
 }
+
+async function getCosmeticById(id) {
+    const query = `SELECT * FROM employees WHERE id = ${id} LIMIT 1`;
+    const result = await db.getConnection.query(query);
+    return result.rows;
+}
+
+async function createCosmetic(cosmetic) {
+    const query = `INSERT INTO cosmetics (name, type) VALUES (?, ?) RETURNING *`;
+    const values = [cosmetic.name, cosmetic.type];
+    const result = await db.getConnection.query(query, values);
+    return result.rows[0];
+}
+
